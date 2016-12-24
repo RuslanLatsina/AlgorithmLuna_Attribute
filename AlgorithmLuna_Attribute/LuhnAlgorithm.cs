75abee6cefc65b1a88d21329f7248440a610c281
Ruslan Latsina <avatarandruslan@gmail.com>
2016-12-24 18:50:29 +0200

    add LuhnAlgorithm class

---
+++ AlgorithmLuna_Attribute/LuhnAlgorithm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmLuna_Attribute
+{
+    public class LuhnAlgorithm
+    {
+        public bool IsValid(object value)
+        {
+            long number;
+            string code = value.ToString();
+            if (value != null && long.TryParse(code, out number))
+            {
+                int digitCount = (int)Math.Log10(number) + 1;
+                if (digitCount == 16)
+                {
+                    long[] array = new long[digitCount];
+                    long sum = 0, p = 0;
+                    for (int i = 0; i < digitCount; i++)
+                    {
+                        array[i] = number % 10;
+                        number /= 10;
+                    }
+
+                    for (int i = 0; i < digitCount; i++)
+                    {
+                        p = array[digitCount - 1 - i];
+                        if (i % 2 == 0)
+                        {
+                            p = 2 * p;
+                            if (p > 9)
+                                p = p - 9;
+                        }
+                        sum = sum + p;
+                    }
+
+                    if (sum % 10 == 0)
+                    {
+                        return true;
+                    }
+
+                    return false;
+                }
+                return false;
+            }
+            return false;
+        }
+    }
+}
--- AlgorithmLuna_Attribute/LuhnCardNumberAttribute.cs
+++ AlgorithmLuna_Attribute/LuhnCardNumberAttribute.cs
@@ -9,46 +9,11 @@ namespace AlgorithmLuna_Attribute
         private const string DefaultErrorMessage = "Incorrect credit card ";
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            long number;
-            string code = value.ToString();
-            if (value != null && long.TryParse(code, out number ))
-            {
-                int digitCount = (int)Math.Log10(number) + 1;
-                if (digitCount == 16)
-                {
-                    long[] array = new long[digitCount];
-                    long sum = 0, p = 0;
-                    for (int i = 0; i < digitCount; i++)
-                    {
-                        array[i] = number % 10;
-                        number /= 10;
-                    }
-                    
-                    for (int i = 0; i < digitCount; i++)
-                    {
-                        p = array[digitCount - 1 - i];
-                        if (i % 2 == 0)
-                        {
-                            p = 2 * p;
-                            if (p > 9)
-                                p = p - 9;
-                        }
-                        sum = sum + p;
-                    }
-
-                    if (sum % 10 == 0)
-                    {
-                        return ValidationResult.Success;
-                    }
-
-                    return new ValidationResult(ErrorMessage ??
-                                            DefaultErrorMessage);
-                }
-                return new ValidationResult(ErrorMessage ??
-                                            DefaultErrorMessage);
-            }
+            var luhnAlgorithm = new LuhnAlgorithm();
+            if(luhnAlgorithm.IsValid(value))
+                return ValidationResult.Success;
             return new ValidationResult(ErrorMessage ??
-                                        DefaultErrorMessage);
+                                            DefaultErrorMessage);
         }
     }
 }
